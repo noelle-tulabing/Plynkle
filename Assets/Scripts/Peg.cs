7cc130ef8a1597ac3b3dff696fd4a87e74d34dc1
Noelle Tulabing <noelle.tulabing@gmail.com>
2026-03-02 19:52:27 -0800

    Added peg behavior, slot triggers

---
+++ Assets/Scripts/Peg.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peg : MonoBehaviour
+{
+   public List<Sprite> Sprites;
+   
+   private int currentSpriteNumber = 0;
+   private SpriteRenderer spriteRenderer;
+
+   public void Start()
+   {
+      spriteRenderer = GetComponent<SpriteRenderer>();
+      spriteRenderer.sprite = Sprites[currentSpriteNumber];
+   }
+   public void OnCollisionEnter2D(Collision2D other)
+   {
+      if (other.gameObject.CompareTag("Ball"))
+      {
+         OnBallHit();
+      }
+   }
+
+   private void OnBallHit()
+   {
+      if (NoMoreSprites())
+      {
+         Destroy(gameObject);
+      }
+      else
+      {
+         ShowNextSprite();
+      }
+   }
+
+   public void ShowNextSprite()
+   {
+      currentSpriteNumber++;
+      spriteRenderer.sprite = Sprites[currentSpriteNumber];
+   }
+
+   private bool NoMoreSprites()
+   {
+      if (currentSpriteNumber == 2)
+      {
+         return true;
+      }
+      return false;
+   }
+}
