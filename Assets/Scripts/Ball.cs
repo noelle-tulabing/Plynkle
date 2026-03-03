e5d31fae822e0aa04698d65cee75c4cadbe937b0
Noelle Tulabing <noelle.tulabing@gmail.com>
2026-03-02 21:18:31 -0800

    Added peg pitch shifting

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,8 +1,25 @@
+using System;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioClip PegHitClip;
+    private AudioSource audioSource;
+
+    public void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Peg"))
+        {
+            audioSource.PlayOneShot(PegHitClip);
+            audioSource.pitch += 0.1f;
+        }
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("SlotTrigger"))
