dbab8524660f7c0ba0f77e056bed4f01b5b0d621
Noelle Tulabing <noelle.tulabing@gmail.com>
2026-03-02 21:40:09 -0800

    Added ring particles

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -4,13 +4,13 @@ public class Slot : MonoBehaviour
 {
    public UI UI;
    public int Points = 10;
+   
    public void OnTriggerEnter2D(Collider2D other)
    {
       if (other.CompareTag("Ball"))
       {
          OnBallEntered(other);
       }
-      
    }
 
    private void OnBallEntered(Collider2D other)
