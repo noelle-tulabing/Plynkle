930ef800bb94170c8fb99bf2d677512fe90de1f1
Noelle Tulabing <noelle.tulabing@gmail.com>
2026-03-02 22:16:13 -0800

    Added fireworks

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -4,7 +4,9 @@ public class Slot : MonoBehaviour
 {
    public UI UI;
    public int Points = 10;
-   
+   public GameObject FireworksParticlePrefab;
+   public Sounds Sounds;
+
    public void OnTriggerEnter2D(Collider2D other)
    {
       if (other.CompareTag("Ball"))
@@ -13,9 +15,29 @@ public class Slot : MonoBehaviour
       }
    }
 
-   private void OnBallEntered(Collider2D other)
+   private void OnBallEntered(Collider2D ball)
    {
       ScorePoint();
+      SpawnParticles(ball);
+      PlayFireworksSound();
+   }
+
+   private void PlayFireworksSound()
+   {
+      Sounds.PlayFireworkSound();
+   }
+
+   private void SpawnParticles(Collider2D ball)
+   {
+      Vector3 particlePosition = GetParticlePosition(ball);
+      Instantiate(FireworksParticlePrefab, particlePosition, Quaternion.identity);
+   }
+
+   private Vector3 GetParticlePosition(Collider2D ball)
+   {
+      Vector3 ballPosition = ball.transform.position;
+      ballPosition.y += 0.25f;
+      return ballPosition;
    }
 
    private void ScorePoint()
